4ed0389f5cdb5d1a0d5e6fcc5c040ccdcf3dabe0
Daniel Russo <daniel.russo@polarmobile.com>
2012-11-24 17:50:21 -0500

    working sensors + motors + servo

--- robotcomposer.ino
+++ robotcomposer.ino
@@ -1,14 +1,24 @@
+#include <Servo.h>
+
 // Config
-boolean SERIAL_ENABLED = true;
-boolean KEYBOARD_ENABLED = true;
+boolean KEYBOARD_ENABLED = false;
+boolean LOG_COMMANDS = false;
+boolean LOG_SENSOR_VALS = true;
 
 // Misc. pin assignments
 int ledPin = 13;
 
-// Sensor pin assignments
-int irPin = 0;
+// Sensor pin assignments -- digital
 int buttonPin = 7;
 int magButtonPin = 4;
+
+// Sensor pin assignments -- analog
+int irPin = 0;
+int flexPin = 1;
+int potentiometerPin = 2;
+
+// Servo pin
+int servoPin = 6;
 
 // Motor pin assigments
 int STBY = 10;
@@ -19,18 +29,24 @@ int MOTOR_B_PWM = 5;
 int MOTOR_B_IN1 = 11;
 int MOTOR_B_IN2 = 12;
 
-// Sensor input values
+// Sensor readings
 int irVal;
 int buttonVal;
 int magButtonVal;
+int flexVal;
+int potentiometerVal;
+
+// Servo setup
+Servo myservo;
+int servoPos = 0;
 
 void setup() {
-  // Pin setup
+  // Digital pin setup
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(magButtonPin, INPUT);
 
-  // Pin setup -- motors
+  // Digigal pin setup -- motors
   pinMode(STBY, OUTPUT);
   pinMode(MOTOR_A_PWM, OUTPUT);
   pinMode(MOTOR_A_IN1, OUTPUT);
@@ -38,6 +54,9 @@ void setup() {
   pinMode(MOTOR_B_PWM, OUTPUT);
   pinMode(MOTOR_B_IN1, OUTPUT);
   pinMode(MOTOR_B_IN2, OUTPUT);
+
+  // Setup the servo
+  myservo.attach(servoPin);
 
   // Initialize the the button pins to the HIGH position.
   // The button is ON when we read a LOW voltage.
@@ -50,18 +69,47 @@ void setup() {
     ; // Wait for serial port to connect. Needed for Leonardo only.
   }
 
+  // Keyboard setup
   Keyboard.begin();
 }
 
-void updateSensorVals() {
+void updateSensorVals(String &logMsg) {
+  // Analog
+  flexVal = map(analogRead(flexPin), 768, 853, 0, 90);
+  potentiometerVal = analogRead(potentiometerPin);
   irVal = analogRead(irPin);
+
+  // Digital
   buttonVal = digitalRead(buttonPin);
   magButtonVal = digitalRead(magButtonPin);
+
+  if (LOG_SENSOR_VALS) {
+    logMsg += "\n\n-- Sensor Values --";
+    logMsg += "\nFlex: ";
+    logMsg += flexVal;
+    logMsg += "\nPontentiometer: ";
+    logMsg += potentiometerVal;
+    logMsg += "\nIR: ";
+    logMsg += irVal;
+    logMsg += "\nButton: ";
+    logMsg += buttonVal;
+    logMsg += "\nMagButton: ";
+    logMsg += magButtonVal;
+  }
 }
 
 void prepareMessage() {
   if (buttonVal == LOW) send(String("wobble + 1"));
   if (magButtonVal == LOW) send(String("wobble reset 1"));
+}
+
+void moveServo() {
+  for(servoPos = 0; servoPos < 180; servoPos += 1) {
+    myservo.write(servoPos);
+  }
+  for(servoPos = 180; servoPos>=1; servoPos-=1) {
+    myservo.write(servoPos);
+  }
 }
 
 void move(int motor, int speed, int direction) {
@@ -102,13 +150,16 @@ void stopMotors() {
 }
 
 void send(const String &msg) {
-  if (SERIAL_ENABLED) Serial.println(msg);
+  if (LOG_COMMANDS) Serial.println(msg);
   if (KEYBOARD_ENABLED) Keyboard.println(msg);
 }
 
 void loop() {
-  updateSensorVals();
+  String logMsg = "";
+  updateSensorVals(logMsg);
   prepareMessage();
   runMotor();
+  moveServo();
+  Serial.println(logMsg);
   delay(500);
 }
